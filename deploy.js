35f8b03495704c460f82435a72e7e1c4d84d74cf
Dylan Grant <dgrant069@hotmail.com>
2018-01-25 22:11:31 -0800

    added gitignore for node modules. made tests actually test the code we wrote but not importing it yet. added a deploy script

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HdWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+
+const env = require('./envs/constants');
+const { interface, bytecode } = require('./compile')
+
+const provider = new HdWalletProvider(
+  env.DEV_MNEMONIC,
+  `https://ropsten.infura.io/${env.INFURA_API_KEY}`
+)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts()
+
+  console.log('accounts', accounts[0])
+  
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' })
+
+  console.log('result from deploy', result)
+  return result
+}
+
+deploy()
