71f71b4ce3a1cf3f025390193b1e633735924212
Dylan Grant <dgrant069@hotmail.com>
2018-01-26 22:29:07 -0800

    saving before removing Inbox stuff

--- deploy.js
+++ deploy.js
@@ -16,7 +16,7 @@ const deploy = async () => {
   console.log('accounts', accounts[0])
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' })
 
   console.log('result from deploy', result)
